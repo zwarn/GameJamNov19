bf72fdd6a4692a728ce573f1bc7966c4139ddf90
David Radobenko <d.radobenko@yahoo.de>
2019-11-09 16:00:06 +0100

    WIP Falling Obstacles

---
+++ Assets/Scripts/ObstacleChooser.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleChooser : MonoBehaviour
+{
+    private Color mouseOverColor = Color.blue;
+    private Color originalColor = Color.white;
+    private bool dragging = false;
+    private bool isDragable = true;
+    private float distance;
+    [SerializableField] private Rigidbody2D rigidbody2D;
+
+    void OnMouseEnter()
+    {
+        GetComponent<Renderer>().material.color = mouseOverColor;
+    }
+ 
+    void OnMouseExit()
+    {
+        GetComponent<Renderer>().material.color = originalColor;
+    }
+ 
+    void OnMouseDown()
+    {
+        if (isDragable)
+        {
+            distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+            dragging = true;
+        }
+    }
+ 
+    void OnMouseUp()
+    {
+        dragging = false;
+        isDragable = false;
+    }
+ 
+    void Update()
+    {
+        if (dragging)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 rayPoint = ray.GetPoint(distance);
+            transform.position = rayPoint;
+        }
+    }
+}
