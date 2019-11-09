15a722a16238c5a64291d0ec737bab5aa2c18cab
ShackMan2000 <julianpieper@gmx.de>
2019-11-09 18:56:10 +0100

    no message

---
+++ Assets/Scripts/CameraFollowsTower.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowsTower : MonoBehaviour
+{
+   
+
+    [SerializeField]
+    private float cameraMoveSpeed;
+
+    public bool moving;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(moving)
+            transform.position += new Vector3 (0f, cameraMoveSpeed * Time.deltaTime, 0f);
+    }
+}
