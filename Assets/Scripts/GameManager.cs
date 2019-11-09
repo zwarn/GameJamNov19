6f3dc8f52239fa673e91019c8d3c200f318f92de
David Radobenko <d.radobenko@yahoo.de>
2019-11-09 19:18:17 +0100

    Created the Gameover screen and Bear respawning

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     private float bearHeight;
+    [SerializeField] private GameObject gameOverScreen;
+    [SerializeField] private GameObject bearPrefab;
+    [SerializeField] private GameObject bearPlayerInstance;
+    [SerializeField] private Transform bearSpawnTransform;
 
     // Start is called before the first frame update
     void Start()
@@ -15,11 +19,23 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            PlayerDies();
+        }
     }
 
     void PlayerDies()
     {
-        
+        Destroy(bearPlayerInstance);
+        gameOverScreen.SetActive(true);
+        StartCoroutine(RespawnPlayer());
+    }
+
+    private IEnumerator RespawnPlayer()
+    {
+        yield return new WaitForSeconds(2.0f);
+        gameOverScreen.SetActive(false);
+        bearPlayerInstance = Instantiate(bearPrefab, bearSpawnTransform.position, Quaternion.identity);
     }
 }
