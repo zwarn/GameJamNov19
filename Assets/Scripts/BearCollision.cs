ff573f0d8868bd64cd36eb3ece2c94fa7997ebe8
Daniel Senff <mail@danielsenff.de>
2019-11-09 19:22:56 +0100

    have bear jump sound on collisions

--- Assets/Scripts/BearCollision.cs
+++ Assets/Scripts/BearCollision.cs
@@ -6,11 +6,11 @@ using UnityEngine;
 public class BearCollision : MonoBehaviour
 {
     public float deadlyVelocity = 10f;
-    
+    public AudioSource bearJumpSource;
     // Start is called before the first frame update
     void Start()
     {
-        
+        bearJumpSource = GetComponent<AudioSource> ();
     }
 
     // Update is called once per frame
@@ -21,7 +21,10 @@ public class BearCollision : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.CompareTag("block") && other.rigidbody != null && other.rigidbody.velocity.magnitude > deadlyVelocity)
+        bearJumpSource.Play();
+        if (other.gameObject.CompareTag("block")
+            && other.rigidbody != null
+            && other.rigidbody.velocity.magnitude > deadlyVelocity)
         {
             Debug.Log("die because of velocity " + other.rigidbody.velocity.magnitude);
             Destroy(gameObject);
