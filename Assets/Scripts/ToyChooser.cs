4340bbbbd44291cbae3ae2824bd92a82e3e46748
zwarn <Michael.Budahn@gmail.com>
2019-11-09 16:36:51 +0100

    Add ToyChooser again

---
+++ Assets/Scripts/ToyChooser.cs
@@ -0,0 +1,34 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToyChooser : MonoBehaviour
+{
+    private static ToyChooser _instance;
+
+    public static ToyChooser GetInstance()
+    {
+        return _instance;
+    }
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+        }
+        else
+        {
+            Debug.LogError("ToyChooser already present");
+        }
+    }
+    
+    public List<GameObject> toys;
+
+    public GameObject getRandomToy()
+    {
+        int choice = Random.Range(0, toys.Count);
+        return toys[choice];
+    }
+    
+}
